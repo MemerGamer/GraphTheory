917896fb6a7dd0a669bb0199be7652a4c20a43f7
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2023-05-24 19:21:01 +0300

    Create useful-includes.h

---
+++ C++/useful-includes.h
@@ -0,0 +1,27 @@
+//
+// Created by hunor on 24.05.2023.
+//
+
+#ifndef C___USEFUL_INCLUDES_H
+#define C___USEFUL_INCLUDES_H
+
+// Useful includes
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <ctime>
+#include <cstdlib>
+#include <limits>
+
+using namespace std;
+
+#endif //C___USEFUL_INCLUDES_H
