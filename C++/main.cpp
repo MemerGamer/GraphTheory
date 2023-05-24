150600e13eb91ad4cc17e0fe143774cf5b37a4ff
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2023-05-24 19:21:54 +0300

    Update main.cpp

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <utility>
+#include "tests.h"
 
-int main() {
+void tests(string input_file_path){
+    test_breadth_first_search(std::move(input_file_path));
+}
+int main(int argc, char** argv) {
     std::cout << "GraphTheory Algorithms" << std::endl;
+    if (argc != 2) {
+        cout << "Usage: ./"<<argv[0]<<" <input_file_path>\n";
+        return 1;
+    }
+    string input_file_path = argv[1];
+    tests(input_file_path);
     return 0;
 }
