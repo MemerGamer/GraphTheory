a1988a681da6c4293c73129e40c09aab6bcf59e3
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2023-08-12 15:58:03 +0300

    feat(tests)<Python>: implemented test function for prufer encoding and decoding

--- Python/tests.py
+++ Python/tests.py
@@ -1,11 +1,13 @@
 import config
 from bfs import BreadthFirstSearch
 from dfs import DepthFirstSearch
+from prufer import Prufer
 
 
 def tests(input_file_path):
     test_breadth_first_search(input_file_path)
     test_depth_first_search(input_file_path)
+    test_prufer(input_file_path)
 
 
 def test_breadth_first_search(input_file_path):
@@ -53,3 +55,30 @@ def test_depth_first_search(input_file_path):
     start_node = 1
     print("DFS traversal starting from node", start_node, ":", end=" ")
     graph.depth_first_search(start_node)
+
+
+def test_prufer(input_file_path):
+    with open(input_file_path, "r") as input_file:
+        number_of_nodes, number_of_edges = map(int, input_file.readline().split())
+
+        prufer_tree = Prufer(number_of_nodes)
+
+        for i in range(number_of_edges):
+            line = input_file.readline()
+            if not line:
+                continue
+            # skip empty lines
+            line = line.strip()  # Read the line
+            # print(line)  # Print the line to verify the contents
+            first_node, second_node = map(int, line.split())  # Use the line
+            prufer_tree.add_edge_nondirected(first_node, second_node)
+
+        print("Prufer sequence:", end=" ")
+        prufer_sequence = prufer_tree.encode_tree()
+        print(" ".join(map(str, prufer_sequence)))
+
+        print("Decoded Prufer sequence:", end=" ")
+        decoded_edges = prufer_tree.decode_prufer_sequence()
+        for edge in decoded_edges:
+            print(f"({edge[0]}, {edge[1]})", end=" ")
+        print()
