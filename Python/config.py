c8909eb9201294e6b046884e42cdf327c8f7fabb
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2023-08-12 15:59:20 +0300

    feat(config)<Python>: added prufer package to path

--- Python/config.py
+++ Python/config.py
@@ -6,6 +6,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 algorithms_dir = os.path.join(current_dir, "algorithms")
 sys.path.append(algorithms_dir)
 
-# Add the 'algorithms.breadth_first_search' package to the Python path
+# Add the packages to the Python path
 sys.path.append(os.path.join(algorithms_dir, "breadth_first_search"))
 sys.path.append(os.path.join(algorithms_dir, "depth_first_search"))
+sys.path.append(os.path.join(algorithms_dir, "prufer_encode_decode"))
