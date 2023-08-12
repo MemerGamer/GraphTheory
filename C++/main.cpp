07e8eb6d9f0432eda3912e8fd74ba3e7851ab6f0
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2023-08-12 15:46:49 +0300

    feat(algorithms)<CPP>: called test prufer

--- C++/main.cpp
+++ C++/main.cpp
@@ -2,14 +2,16 @@
 #include <utility>
 #include "tests.h"
 
-void tests(string input_file_path){
+void tests(string input_file_path) {
     test_breadth_first_search(std::move(input_file_path));
     test_depth_first_search(std::move(input_file_path));
+    test_prufer(std::move(input_file_path));
 }
-int main(int argc, char** argv) {
+
+int main(int argc, char **argv) {
     std::cout << "GraphTheory Algorithms" << std::endl;
     if (argc != 2) {
-        cout << "Usage: ./"<<argv[0]<<" <input_file_path>\n";
+        cout << "Usage: ./" << argv[0] << " <input_file_path>\n";
         return 1;
     }
     string input_file_path = argv[1];
